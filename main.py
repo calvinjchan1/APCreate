f50ae62c3792ba206f88c29507cb8b5016d92ef2
calvinjchan1 <calvinjchan1@gmail.com>
2019-03-27 10:39:44 -0700

    Created basic structure for main

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-import pygame
+import pygame, sys
+
+#Config
+global WINDOW_DIMENSIONS
+WINDOW_DIMENSIONS = (640,450)
+
+
+def start():
+    '''
+    Run once at the beginning of the program
+    '''
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_DIMENSIONS)
+
+def mainLoop():
+    '''
+    The main loop of the program
+    '''
+    def handleEvents():
+        '''
+        Deals with all of the user input
+        '''
+        
+        for event in pygame.event.get():
+            #Exit the game
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    while(True):
+        handleEvents()
+    
+
+def main():
+    start()
+    mainLoop()
+
+main()
